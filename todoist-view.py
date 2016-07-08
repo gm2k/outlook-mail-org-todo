7f351570605284156ef8cf19961ad364ca8eeb3f
Gene Mamonchikov <gm2k@yandex.ru>
2016-07-08 16:51:45 +0300

    get projects list

--- todoist-view.py
+++ todoist-view.py
@@ -1,8 +1,23 @@
-import todoist
+from pytodoist.api import TodoistAPI
+from pytodoist import todoist
 import sys
+import os
 
-# Todoist API - first argument in commandline
-api = todoist.TodoistAPI(sys.argv)
-response = api.sync()
-for project in response['Projects']:
-    print(project['name'])
+user_login = sys.argv[1]
+user_password = sys.argv[2]
+org_folder = sys.argv[3]
+user = todoist.login(user_login, user_password)
+
+def main():
+    projects = user.get_projects()
+    for project in projects:
+        print("* "+project.name)
+        tasks = project.get_tasks()
+        for task in tasks:
+            print("**" + task.content.encode('cp866','replace'))
+
+
+
+
+if __name__ == '__main__':
+    main()
